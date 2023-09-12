704f7d2d9fd347b6ebc2c479fa00c410699a21c1
Dan <dan@eighttrigrams.net>
2023-09-12 19:46:08 +0100

    Fix missing alias

--- src/clj/repository.clj
+++ src/clj/repository.clj
@@ -139,7 +139,7 @@
       {:selected-context selected-context})))
 
 (defn load-stored-context [{:keys [db]}]
-  (fn [{:keys [selected-context]} idx]
+  (fn [{:keys [selected-context] :as opts} idx]
     (let [selected-context (datastore/load-stored-context db selected-context idx)]
       {:selected-context selected-context
        :issues (search/search-issues db (assoc opts :selected-context selected-context))})))
