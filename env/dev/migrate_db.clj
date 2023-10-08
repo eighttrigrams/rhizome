4c1c24c4a518f2982ef630f1f9b5489bb3dcd37e
Dan <dan@eighttrigrams.net>
2023-10-08 18:15:47 +0100

    Tweak script

--- env/dev/migrate_db.clj
+++ env/dev/migrate_db.clj
@@ -2,17 +2,19 @@
   (:require [next.jdbc :as jdbc]
             [honey.sql :as sql]
             [datastore.contexts :as contexts]
+            datastore
             [datastore.issues :as issues]
             [datastore.search :as search]))
 
-(defn- migrate-single-context [db {:keys [title] :as context} context-id]
+(defn- migrate-single-context [db {:keys [id title] :as context} context-id]
   (let [issue (issues/new-issue db {:title title} context-id #{} false)
         new-issue {:issue (merge 
                            (select-keys context [:tags :short_title])
                            (select-keys issue [:id :title]))}]
     (issues/update-issue
      db
-     new-issue)))
+     new-issue)
+    (datastore/delete-context db {:id id})))
 
 (comment
   (let [db  (:db (read-string (slurp "./config.edn")))
