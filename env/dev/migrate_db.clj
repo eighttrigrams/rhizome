c73de23554ba13e6a44a9393ad7bb1497d5a7140
Dan <dan@eighttrigrams.net>
2023-10-08 18:05:11 +0100

    Work on migration script

--- env/dev/migrate_db.clj
+++ env/dev/migrate_db.clj
@@ -2,13 +2,26 @@
   (:require [next.jdbc :as jdbc]
             [honey.sql :as sql]
             [datastore.contexts :as contexts]
+            [datastore.issues :as issues]
             [datastore.search :as search]))
 
+(defn- migrate-single-context [db {:keys [title] :as context} context-id]
+  (let [issue (issues/new-issue db {:title title} context-id #{} false)
+        new-issue {:issue (merge 
+                           (select-keys context [:tags :short_title])
+                           (select-keys issue [:id :title]))}]
+    (issues/update-issue
+     db
+     new-issue)))
+
 (comment
-  (let [db  (:db (read-string (slurp "./config.edn")))]
-    
+  (let [db  (:db (read-string (slurp "./config.edn")))
+       context (first (filter #(not= "migrated" (:title %)) (search/search-contexts db {})))
+        {:keys [id]} (contexts/new-context db {:title "migrated"})]
+    (prn (migrate-single-context db context id))
     
     #_(jdbc/execute! db (sql/format {:select :contexts.*
                                      :from [:contexts]
+                                     
                                      :limit 1}))
-    (first (search/search-contexts db {}))))
+    ))
