6fe06bf81f741c6388fa0064263b34d257cd37c4
Dan <dan@eighttrigrams.net>
2024-01-02 17:07:18 +0000

    Only confirm once

--- src/cljs/ui/actions.cljs
+++ src/cljs/ui/actions.cljs
@@ -209,6 +209,4 @@
 
 (defn delete-context! [*state]
   (when (js/window.confirm "Delete currently selected context?")
-    (when (js/window.confirm "Sure?")
-      (when (js/window.confirm "Really?")
-        (fetch-and-reset-with-method! *state @*state api/delete-context (:selected-context @*state))))))
+    (fetch-and-reset-with-method! *state @*state api/delete-context (:selected-context @*state))))
