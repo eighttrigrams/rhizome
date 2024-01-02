b8fd86d2a71d2f353d1416c740ce0c6a01271ea0
Dan <dan@eighttrigrams.net>
2024-01-02 18:11:51 +0000

    Allow markdown in context headers

--- src/cljs/ui/main/lhs/list_item.cljs
+++ src/cljs/ui/main/lhs/list_item.cljs
@@ -1,6 +1,13 @@
 (ns ui.main.lhs.list-item
-  (:require [ui.actions :as actions]
+  (:require ["react-markdown$default" :as ReactMarkdown]
+            [ui.actions :as actions]
             [ui.main.context-badges :as context-badges]))
+
+;; TODO dedup with title-component form issues-list-items
+(defn title-component [title]
+  [:span.title
+   [:> ReactMarkdown
+    {:children title}]])
 
 (defn component [*state context]
   [:li.card.issue-card
@@ -8,5 +15,5 @@
                        (:id context)) :selected)
     :on-click #(actions/select-context! *state context)}
    [:div
-    [:span.title.title1 (:title context)]
+    [title-component (:title context)]
     [context-badges/component (:contexts context)]]])
