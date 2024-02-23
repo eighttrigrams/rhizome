9f99b398a0c08b27f6d7bcc55d7f7615172a7f9a
Dan <dan@eighttrigrams.net>
2024-02-23 14:52:12 +0000

    Simplify

--- src/clj/repository/insertion/substack.clj
+++ src/clj/repository/insertion/substack.clj
@@ -12,38 +12,49 @@
                                                    (select/class "post-title")) tree))))]
     title))
 
+(defn- get-substack-id 
+  [db 
+   [subdomain-url 
+    subdomain-handle 
+    subdomain-full-url] 
+   substacks-id]
+  (let [substack (datastore/new-issue db 
+                                      subdomain-url
+                                      subdomain-handle
+                                      substacks-id
+                                      #{})
+        substack (datastore/update-issue db
+                                         {:issue              (update substack :data
+                                                                      (fn [data] (assoc data :resource-links {:substack subdomain-full-url})))
+                                          :related-issues-ids '()})]
+    (:id (datastore/upgrade-issue-to-context! db substack))))
+
+(defn-  convert [url]
+  (let [idx (str/index-of url ".substack")
+          su (subs url 0 idx)
+          subdomain (str/replace su "https://" "")
+          subdomain-handle (str subdomain ".substack")
+          subdomain-url (str subdomain-handle ".com")
+          subdomain-full-url (str "https://" subdomain-url)]
+    [subdomain-handle
+     subdomain-url
+     subdomain-full-url]))
+
 (defn save-article [db url selected-context-id]
   (let [substacks-id (:id (get-item/get-item-by-title db {:title "Substacks"}))
         articles-id (:id (get-item/get-item-by-title db {:title "Articles"}))]
-    (tap> [:----------------])
     (when-not substacks-id (throw (Exception. "no substacks-id")))
     (when-not articles-id (throw (Exception. "no articles-id")))
-    (let [idx (str/index-of url ".substack")
-          su (subs url 0 idx)
-          subdomain (str/replace su "https://" "")
-          subdomain-url (str subdomain ".substack.com")
-          subdomain-full-url (str "https://" subdomain-url)]
-      (let [channel-handle (str subdomain ".substack")
-            substack-id (:id (get-item/get-item-by-substack db subdomain-full-url))
-            substack-id (or substack-id
-                            (let [substack (datastore/new-issue db 
-                                                                subdomain-url
-                                                                channel-handle
-                                                                substacks-id
-                                                                #{})
-                                  substack (datastore/update-issue db
-                                                                   {:issue              (update substack :data
-                                                                                                (fn [data] (assoc data :resource-links {:substack subdomain-full-url})))
-                                                                    :related-issues-ids '()})]
-                              (:id (datastore/upgrade-issue-to-context! db substack))))]
-        (let [title (get-post-title url)
-              _ (when-not title (throw (Exception. "no post title")))
-              issue (datastore/new-issue db 
-                                         (get-post-title url)
-                                         ""
-                                         selected-context-id
-                                         #{substack-id articles-id})
-              issue (datastore/update-issue db {:issue (update issue :data 
-                                                               (fn [data] (assoc data :resource-links {:substack-article url})))
-                                                :related-issues-ids '()})]
-        issue)))))
+    (let [identifiers (convert url)
+          substack-id (:id (get-item/get-item-by-substack db (last identifiers)))
+          substack-id (or substack-id (get-substack-id db identifiers substacks-id))
+          title       (get-post-title url)
+          _           (when-not title (throw (Exception. "no post title")))
+          issue       (datastore/new-issue db 
+                                           (get-post-title url)
+                                           ""
+                                           selected-context-id
+                                           #{substack-id articles-id})]
+      (datastore/update-issue db {:issue              (update issue :data 
+                                                              (fn [data] (assoc data :resource-links {:substack-article url})))
+                                  :related-issues-ids '()}))))
