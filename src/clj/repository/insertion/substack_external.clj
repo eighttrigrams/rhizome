a9accba9145df8fedd167aea21206e1a57c3af01
Dan <dan@eighttrigrams.net>
2024-02-25 01:18:34 +0000

    .

--- src/clj/repository/insertion/substack_external.clj
+++ src/clj/repository/insertion/substack_external.clj
@@ -2,7 +2,9 @@
   (:require [repository.insertion.substack :as substack]))
 
 (defn match? [title]
-  (re-matches #"https://www.arktosjournal.com\/p\/.*" title))
+  (or (re-matches #"https://www.arktosjournal.com\/p\/.*" title)
+      (re-matches #"https://www.eugyppius.com\/p\/.*" title)
+      (re-matches #"https://www.alexkaschuta.com\/p\/.*" title)))
 
 (defn save-article [db url context-ids-set should-capture-summary?]
   ((substack/make:save-article true) db url context-ids-set should-capture-summary?))
