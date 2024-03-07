71bacb062d873769896de4863cd0e3f1ac871caa
Dan <dan@eighttrigrams.net>
2024-03-07 23:03:31 +0000

    Optimizations round 1

--- src/clj/datastore/search.clj
+++ src/clj/datastore/search.clj
@@ -158,7 +158,13 @@
     all-issues-ids))
 
 (defn- issues-query [ids]
-  {:select   [:issues.*
+  {:select   [:issues.title
+              :issues.short_title
+              :issues.short_title_ints
+              :issues.id
+              :issues.data
+              :issues.is_context
+              :issues.updated_at
               {:select :date
                :from   [:events]
                :where  [:= :events.issue_id :issues.id]}
