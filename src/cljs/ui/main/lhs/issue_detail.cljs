e8efa14493ed22ee6069d339a9047feb70ad168a
Dan <dan@eighttrigrams.net>
2024-03-10 01:23:17 +0000

    .

--- src/cljs/ui/main/lhs/issue_detail.cljs
+++ src/cljs/ui/main/lhs/issue_detail.cljs
@@ -43,8 +43,8 @@
 (defn- image-component [title data]
   (let [resource-links (:resource-links data)]
     [:<>
-     (when-let [preview-image-link (and (:preview-image data)
-                                        (not (:youtube-video (:resource-links data))))]
+     (when-let [preview-image-link (and (not (:youtube-video (:resource-links data)))
+                                        (:preview-image data))]
        [image-itself (str "Preview/" preview-image-link)])
      (when-let [image-link (:image resource-links)]
        [image-itself image-link])
