d6ef9a326064cd0011f440128bdff3801c9fb572
Dan <dan@eighttrigrams.net>
2024-05-06 11:20:33 +0100

    .

--- src/clj/repository/insertion/common.clj
+++ src/clj/repository/insertion/common.clj
@@ -1,6 +1,8 @@
 (ns repository.insertion.common
   (:require datastore
-            [datastore.get-item :as get-item]))
+            [datastore.get-item :as get-item]
+            [next.jdbc :as jdbc]
+            [honey.sql :as sql]))
 
 (defn get-item-or-throw-error [db title]
   (let [id (:id (get-item/get-item-by-title db {:title title}))
@@ -9,12 +11,22 @@
 
 (defn insert-item 
   [db title short-title context-ids-set resource-links]
-  (let [issue (datastore/new-issue db 
-                                   title
-                                   short-title
-                                   context-ids-set)
-          issue (datastore/update-issue db 
-                                        {:issue              (update issue :data 
-                                                                     (fn [data] (assoc data :resource-links resource-links)))
-                                         :related-issues-ids '()})]
+  (let [issue    (datastore/new-issue db 
+                                      title
+                                      short-title
+                                      context-ids-set)
+        contexts (doall (->> (jdbc/execute! db (sql/format {:select [:id :short_title :title]
+                                                            :from   [:issues]
+                                                            :where  [:in :issues.id 
+                                                                     [:inline (seq context-ids-set)]]}))
+                             (map (fn [{:issues/keys [id short_title title]}]
+                                    [id (if (seq short_title) short_title title)]))
+                             (into {})))
+        issue    (datastore/update-issue db 
+                                         {:issue (update issue :data 
+                                                         (fn [data] 
+                                                           (assoc data 
+                                                                  :resource-links resource-links
+                                                                  :contexts contexts)))
+                                          :related-issues-ids '()})]
     issue))
