e6bd38b322454d820e1747afca4a4e1bc3c54d89
Dan <dan@eighttrigrams.net>
2024-05-08 13:33:17 +0100

    .

--- src/clj/datastore/search.clj
+++ src/clj/datastore/search.clj
@@ -373,18 +373,21 @@
   (sectime
    "search-issues"
    (try
-             (let [opts (
+     (let [opts (
                 ;; TODO instead of doing this, make sure q is always at least ""
-                         if (:q opts) 
-                          (update opts :q remove-some-chars)
+                 if (:q opts) 
+                  (update opts :q remove-some-chars)
                  ;; for destructuring in searcj-issues' to work properly when :q is present but has nil value
-                          (dissoc opts :q))]
-               (if-not selected-context
-                 [(search-issues' db opts) {}]
-                 [(search-issues' db opts) 
-                  (get-aggregated-contexts db 
-                                           opts 
-                                           highlighted-secondary-contexts)]))
-             (catch Exception e
-               (log/error (str "error in search-issues: " (.getMessage e) " - params were: " (with-out-str (pp/pprint opts))))
-               (throw e)))))
+                  (dissoc opts :q))
+           f1 (when selected-context 
+                (future (search-issues' db opts)))
+           f2 (when selected-context 
+                (future (get-aggregated-contexts db 
+                                                 opts 
+                                                 highlighted-secondary-contexts)))]
+       (if-not selected-context
+         [(search-issues' db opts) {}]
+         [@f1 @f2]))
+     (catch Exception e
+       (log/error (str "error in search-issues: " (.getMessage e) " - params were: " (with-out-str (pp/pprint opts))))
+       (throw e)))))
