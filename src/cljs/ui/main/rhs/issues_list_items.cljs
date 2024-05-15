7abacd7ea1bca1fec985f87bd973766f33827455
Dan <dan@eighttrigrams.net>
2024-05-15 21:54:08 +0100

    .

--- src/cljs/ui/main/rhs/issues_list_items.cljs
+++ src/cljs/ui/main/rhs/issues_list_items.cljs
@@ -5,21 +5,6 @@
             [ui.main.context-badges :as context-badges]
             [ui.main.rhs.modifiers :as modifiers]))
 
-(defn info-component [state issue]
-  [:span.info
-   (when (and (:selected-context state)
-              (or (nil? (:events-view (:current (:views (:data (:selected-context state))))))
-                  (= 0 (:events-view (:current (:views (:data (:selected-context state)))))))
-              #_(not= 0 (:search_mode (:selected-context state)))
-              (not (and (= (:short_title_ints issue) 0)
-                        (empty? (:short_title issue)))))
-     (str "["
-          (if (> (:short_title_ints issue) 0)
-            (:short_title_ints issue)
-            (:short_title issue))
-          "] "))
-   [:span.date (:date issue)]])
-
 ;; TODO extract ns
 (defn title-component [title data]
   [:span.title
@@ -147,19 +132,17 @@
       [:div.issue-card-inner-right.issue-card-inner-child
        [title-component (:title issue) (:data issue)]
        (when-not simple-card?
-         [:<>
-          [info-component @*state issue]
-          [context-badges/component (remove #(= (:id (:selected-context @*state))
-                                                (first %)) 
-                                            (merge (when (:is_context issue) 
-                                                     {0 #(actions/select-context! *state issue)})
-                                                   (when (:date issue)
-                                                     {:date (:date issue)})
-                                                   (when (and (:selected-context @*state)
-                                                              (or (nil? (:events-view (:current (:views (:data (:selected-context @*state))))))
-                                                                  (= 0 (:events-view (:current (:views (:data (:selected-context @*state)))))))
-                                                              (> (:short_title_ints issue) 0))
-                                                     {:number (:short_title_ints issue)})
-                                                   (when-let [file (:file (:resource-links (:data issue)))]
-                                                     {:file file})
-                                                   (:contexts (:data issue))))]])]]]))
+         [context-badges/component (remove #(= (:id (:selected-context @*state))
+                                               (first %)) 
+                                           (merge (when (:is_context issue) 
+                                                    {0 #(actions/select-context! *state issue)})
+                                                  (when (:date issue)
+                                                    {:date (:date issue)})
+                                                  (when (and (:selected-context @*state)
+                                                             (or (nil? (:events-view (:current (:views (:data (:selected-context @*state))))))
+                                                                 (= 0 (:events-view (:current (:views (:data (:selected-context @*state)))))))
+                                                             (> (:short_title_ints issue) 0))
+                                                    {:number (:short_title_ints issue)})
+                                                  (when-let [file (:file (:resource-links (:data issue)))]
+                                                    {:file file})
+                                                  (:contexts (:data issue))))])]]]))
