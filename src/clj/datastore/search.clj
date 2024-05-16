a0c0f24c8978198738b8515a52d4d14889f126bc
Dan <dan@eighttrigrams.net>
2024-05-16 12:41:44 +0100

    .

--- src/clj/datastore/search.clj
+++ src/clj/datastore/search.clj
@@ -168,18 +168,20 @@
 
 (defn- re-order [search-mode issues]
   (if (= 2 search-mode)
-    (let [top (sort-by #(:short_title %) 
+    (let [_top (sort-by #(:short_title %) 
                        (filter #(and (some? (:short_title %))
                                      (= 0 (:short_title_ints %))) issues))
           bottom (sort-by #(:short_title_ints %)
                           (filter #(> (:short_title_ints %) 0) issues))]
-      (concat top bottom))
+      #_(concat top bottom)
+      bottom)
     (let [top (reverse (sort-by #(:short_title_ints %)
                                 (filter #(> (:short_title_ints %) 0) issues)))
-          bottom (reverse (sort-by #(:short_title %)
+          _bottom (reverse (sort-by #(:short_title %)
                                    (filter #(and (= (:short_title_ints %) 0)
                                                  (some? (:short_title %))) issues)))]
-      (concat top bottom))))
+      #_(concat top bottom)
+      top)))
 
 (defn- expired-filter [issue]
   (and
