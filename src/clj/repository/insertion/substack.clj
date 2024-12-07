7a6e103798e87128229c152aea5813a174f4d33d
Dan <dan@eighttrigrams.net>
2024-12-07 23:56:23 +0000

    .

--- src/clj/repository/insertion/substack.clj
+++ src/clj/repository/insertion/substack.clj
@@ -3,6 +3,8 @@
             datastore
             [cambium.core :as log]
             [hickory.select :as select]
+            [hickory.core :as html]
+            [clj-http.client :as http]
             [repository.insertion.common :as common]
             [repository.chatgpt :as chatgpt]
             utils
@@ -55,8 +57,6 @@
                                                      substacks-id))]
     substack-id))
 
-
-
 (defn- validate-preconditions [db url title]
   (let [_ (when-not (seq title) (throw (Exception. "no post title")))
         _ (when (:id (datastore/get-item-by-path db 
@@ -72,13 +72,66 @@
      (select/and (select/tag "div")
                  (select/class "available-content")) hickory-tree))))))
 
+(defn- get-property [tree name]
+   (-> (select/select (select/attr "property" (fn [x] (= x name))) tree)
+       first
+       :attrs
+       :content
+       str/trim))
+
+(defn- convert-month [month]
+  (get {"Jan" "01"
+        "Feb" "02"
+        "Mar" "03"
+        "Apr" "04"
+        "May" "05"
+        "Jun" "06"
+        "Jul" "07"
+        "Aug" "08"
+        "Sep" "09"
+        "Oct" "10"
+        "Nov" "11"
+        "Dec" "12"} month))
+
+(defn- convert-date [date]
+  (let [[month day year] (filter #(not-empty %) (str/split date #"[\s,]"))]
+    [(str year  "-" (convert-month month) "-" day) year]))
+
+(defn- extract-date [tree]
+   (let [base (select/select (select/descendant (select/class "post-header")
+                                                (select/tag "div")) tree)]
+     (doall (->> base
+                 (filter (fn [item]
+                           (string? (first (:content item)))
+                           #_(string? (first (:content item)))))
+                 (map (fn [item]
+                        (first (:content item))))
+                 (filter (fn [item]
+                           (re-matches #"[A-Z][a-z]{2,4}\s\d\d,\s\d\d\d\d" item)))
+                 first))))
+
+(defn get-post [url extract-content]
+  (let [tree (html/as-hickory (html/parse (:body (http/get url))))
+        title (get-property tree "og:title")
+        subtitle (get-property tree "og:description")
+        date (-> tree extract-date convert-date)]
+    [(str title " - " subtitle) 
+     date
+     (-> tree 
+         extract-content  
+         utils/extract-text)]))
+
+(comment
+  (get-post "https://woodfromeden.substack.com/p/the-anti-autism-manifesto" extract-content))
+
 (defn make:save-article [external?]
   (fn save-article [db url context-ids-set should-capture-summary?]
     (let [url                  (url/url-without-query-params url)
           substack-platform-id (common/get-item-or-throw-error db "Substack")
           substacks-id         (common/get-item-or-throw-error db "Substacks")
           articles-id          (common/get-item-or-throw-error db "Articles")
-          [title content]      (utils/get-post url extract-content)
+          [title [date year] content]      (get-post url extract-content)
+          year-id              (common/get-item-or-throw-error db year)
           _                    (validate-preconditions db url title)
           summary              (and should-capture-summary?
                                     (chatgpt/get-summary content))
@@ -92,13 +145,15 @@
           context-ids-set     (conj context-ids-set
                                     (or substack-id articles-id) ;; hack 
                                     articles-id 
-                                    substack-platform-id)
+                                    substack-platform-id
+                                    year-id)
           _ (log/info (str "context-ids-set: " context-ids-set))
           item                (common/insert-item db 
                                                   title 
                                                   "" 
                                                   context-ids-set 
-                                                  {:substack-article url})]
+                                                  {:substack-article url})
+          item                (datastore/insert-date db (:id item) date nil)]
       (log/info (str "created new item" item))
       (if (and item summary)
         (datastore/update-item db (assoc item :description 
