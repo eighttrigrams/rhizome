6bd667a8e4d52b677393279567a8899b50f2943c
Dan <dan@eighttrigrams.net>
2025-07-14 13:03:47 +0100

    Sort numbers

--- src/cljs/ui/main/rhs/issues_list_items.cljs
+++ src/cljs/ui/main/rhs/issues_list_items.cljs
@@ -153,8 +153,8 @@
                                                      (when (and (:selected-context @*state)
                                                                 (or (= 2 (:search-mode (:current (:views (:data (:selected-context @*state))))))
                                                                     (= 3 (:search-mode (:current (:views (:data (:selected-context @*state)))))))
-                                                                (> (:short_title_ints issue) 0))
-                                                       {:number {:number (:short_title_ints issue)}})
+                                                                (> (:sort_idx issue) 0))
+                                                       {:number {:number (:sort_idx issue)}})
                                                      (when-let [file (:file (:resource-links (:data issue)))]
                                                        {:file {:file file}})
                                                      (:contexts (:data issue))))]]]])
