79df3f22bfe8f7fc398223701b096989aba8c51b
Dan <dan@eighttrigrams.net>
2025-07-17 17:18:53 +0100

    Swap

--- src/cljs/ui/actions/common.cljs
+++ src/cljs/ui/actions/common.cljs
@@ -91,6 +91,7 @@
 
 (defn fetch-and-reset-with-method-2!
   [*state state method & args]
+  (swap! *state assoc :aggregated-contexts nil)
   (apply fetch-and-reset-with-method! *state state method args)
   (let [sequence-number (swap! aggregated-contexts-sequence inc)]
     (js/setTimeout (fn []
