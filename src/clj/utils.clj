bf4785eee8d844eaf99c8481b0b60e0ca1c3bc38
Dan <dan@eighttrigrams.net>
2026-05-15 16:09:26 +0100

    Remove gpt stuff

--- src/clj/utils.clj
+++ src/clj/utils.clj
@@ -1,14 +1,3 @@
-(ns utils
-  (:require [repository.chatgpt :as chatgpt]))
+(ns utils)
 
 (defn condx [p & pairs] (first (keep (fn [[v f]] (when (p v) f)) (partition 2 pairs))))
-
-(defn wrap-summary
-  [summary]
-  (str "--- ChatGPT | "
-       (:model (chatgpt/configuration))
-       " | BEGIN ---\n\n"
-       summary
-       "\n\n--- ChatGPT | "
-       (:model (chatgpt/configuration))
-       " | END ---"))
